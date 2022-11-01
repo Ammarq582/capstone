57d5b34e171a630ce5b9493c3311cb414573d436
Ammarq582 <ammarq582@gmail.com>
2022-11-01 01:55:32 +0000

    Update cart-dropdown.component.jsx and cart-dropdown.styles.jsx

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,50 +1,45 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { CartContext } from '../../contexts/cart.context';
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
-import './cart-dropdown.styles.jsx';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../contexts/cart.context";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import "./cart-dropdown.styles.jsx";
+import {
+  CartDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from "./cart-dropdown.styles.jsx";
 
 const totalPrice = (cartItems) => {
-    return cartItems.reduce((prev, item) => {
-        return prev + (item.quantity * item.price);
-    }, 0)
-}
+  return cartItems.reduce((prev, item) => {
+    return prev + item.quantity * item.price;
+  }, 0);
+};
 
 const CartDropdown = () => {
-    const {cartItems} = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const goToCheckoutHandler = () => {
-        navigate('/checkout')
-    }
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
 
+  return (
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <EmptyMessage>Cart is Empty</EmptyMessage>
+        )}
+      </CartItems>
 
-
-    return(
-        <div className="cart-dropdown-container">
-            <div className="cart-items">
-                {
-                    cartItems.length
-                    ?
-                    cartItems.map(cartItem => (
-                        <CartItem key={cartItem.id} cartItem={cartItem}/>
-                    ))
-                    :
-                    <span className='empty-message'>Cart is Empty</span>
-                }
-                
-            </div>
-
-            <Button onClick={goToCheckoutHandler}>Go to checkout</Button>
-            
-                
-            
-            
-        </div>
-
-    )
+      <Button onClick={goToCheckoutHandler}>Go to checkout</Button>
+    </CartDropdownContainer>
+  );
 };
 
 export default CartDropdown;
